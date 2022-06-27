1e6cc73e84d9ed6287567735a9f0a2cc9218618a
Charliedev-11095 <carlosroyiga@gmail.com>
2022-06-27 18:04:01 -0500

    models updated

--- cursos/models.py
+++ cursos/models.py
@@ -1,21 +1,36 @@
 from django.db import models
-
 # Create your models here.
 class cursos(models.Model): #Define la estructura de nuestra tabla
     idcurso = models.SmallIntegerField(primary_key=True,verbose_name="Id del curso") #Id generado automaticamente
     nombre = models.TextField(max_length=30,verbose_name="Nombre del curso") #Campo de texto 
     descripcion = models.TextField(verbose_name="Características del curso") #Campo de texto
     activo = models.BooleanField(verbose_name="Disponibilidad del curso") #Campo Booleano
-    duracion = models.PositiveSmallIntegerField(verbose_name="Tiempo del curso en meses") #Int pequeño
+    duracion = models.PositiveSmallIntegerField(verbose_name="Tiempo del curso en meses") #Campo de número
     created = models.DateField(auto_now_add=True) 
     updated = models.DateField(auto_now_add=True) 
-
     class Meta:
         verbose_name = "curso"
         verbose_name_plural = "cursos"
         ordering = ["-created"]
-        #El - Ordenado del más reciente a más nuevo
-    
+        #Ordenado de forma descendente
     def __str__(self):
-        return self.nombre
-        #Mostrado por nombre
+        return self.nombre#Mostrado por nombre
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
