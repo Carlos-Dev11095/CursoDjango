d3b3ce01dd81eed841326f02c64271182ff949c5
Charliedev-11095 <carlosroyiga@gmail.com>
2022-06-29 08:59:40 -0500

    Cambios en tabla

--- cursos/admin.py
+++ cursos/admin.py
@@ -6,10 +6,10 @@ from cursos.models import Cursos
 
 class AdministrarModeloCursos(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
-    list_display = ('idcurso', 'nombre', 'activo')
-    search_fields = ('idcurso','nombre','activo',)
+    list_display = ('IdCurso', 'nombreCurso', 'Cupo')
+    search_fields = ('IdCurso','nombreCurso','Cupo',)
     date_hierarchy = 'created'
-    list_filter = ('nombre','activo')
+    list_filter = ('nombreCurso','Cupo')
 
 admin.site.register(Cursos,AdministrarModeloCursos)
 
--- cursos/models.py
+++ cursos/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Cursos(models.Model): #Define la estructura de nuestra tabla
-    idcurso = models.SmallIntegerField(primary_key=True,verbose_name="Clave Curso") #principal auto
-    nombre = models.TextField(max_length=30,verbose_name="Nombre del Curso") #Texto largo limitado
-    descripcion = models.TextField(verbose_name="Descripción del Curso") #Texto largo
-    activo =models.TextField(max_length=30,verbose_name="Estado del curso") #Texto largo limitado
-    cupo = models.PositiveSmallIntegerField(verbose_name="Cupos Disponibles") #Int pequeño
+    IdCurso = models.SmallIntegerField(primary_key=True,verbose_name="Id curso")
+    nombreCurso = models.TextField(max_length=30,verbose_name="Nombre del Curso")
+    Descripcion = models.TextField(verbose_name="Descripción del Curso")
+    activo = models.TextField(max_length=30,verbose_name="Estado del curso") 
+    Cupo = models.PositiveSmallIntegerField(verbose_name="Cupos Disponibles")
     imagen = models.ImageField(null=True,upload_to="fotos",verbose_name="Fotografía")
     created = models.DateField(auto_now_add=True) 
     updated = models.DateField(auto_now_add=True) 
@@ -18,5 +18,5 @@ class Cursos(models.Model): #Define la estructura de nuestra tabla
         #El - Ordenado del más reciente a más nuevo
     
     def __str__(self):
-        return self.nombre
+        return self.nombreCurso
         #Mostrado por nombre
